50770dbec5dfa3d195243b301cbdbab1a757260a
ashuydv02 <ay545153@gmail.com>
2024-09-05 19:08:43 +0530

    added all files

---
+++ techer.py
@@ -0,0 +1,36 @@
+# lst=[23,45,6,34,67,34,67,34,7,86,8,5,6,9,734,78]
+# l1=[]
+# for i in lst:
+#     if i%5==0:
+#         l1.append(i)
+# print(l1)
+# s='i am from a satna district'
+# d={}
+# for i in s:
+#     if i in d:
+#         d[i]=d[i]+1
+#     else:
+#         d[i]=1
+# print(d)
+# s='i am from a satna district'
+# s=s.split()
+# print(s)
+# s='i am from a satna district'
+# v='aeiou'
+# c=0
+# for i in s:
+#     if i in v:
+#         c=c+1
+# print(c)
+# s='i am from a satna district'
+# v='aeiou'
+# c=0
+# for i in s:
+#     if i in v:
+#         pass
+#     else:
+#         c=c+1
+# print(c)
+
+
+
